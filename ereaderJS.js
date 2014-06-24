c106c8f8726dd5e3a0ece20ee436c2e77c73b1b7
hoang nguyen <hvn1@zips.uakron.edu>
2014-06-24 13:31:04 -0400

    Set up page width.

--- ereaderJS.js
+++ ereaderJS.js
@@ -20,7 +20,6 @@ function page_dimensions(width, height) {
 		p_width = width;
 		p_height = height;
 	}
-
 	//if the width is greater than the height we can assume the screen is in landscape mode
 	// in this case we should only use part of the screen for the page
 	else {
@@ -29,25 +28,64 @@ function page_dimensions(width, height) {
 	}
 
 	return [p_width, p_height];
-}
+};
 
 //To use ereader, you must assign an area with the id="ereaderJS"
-$(document).ready(function() {
+var er_page_setup = function(p_width, p_height, page_id) {
 	//variable for textarea containing the body of text
-	var ereader = $('#ereaderJS');
+	var ereader = $(page_id);
 
 	//get text content
-	var manuscript = ereader.val();
+	var manuscript = ereader.val();;
+
+	var er_output = $('#er_output');
+
+	//set up the page
+	$('body').css({
+		"margin" : "0",
+    	"padding" : "0"
+	});
+
+	er_output.css({
+		"border-color": "black",
+		"border-style" : "solid",
+		"border-width" : "1px",
+		"backgroundColor": "white",
+		"width" : p_width,
+		"height": p_height,
+		"overflow": "auto",
+		"margin-left": "auto",
+		"margin-right": "auto"
+	});
+
+	er_output.resizable({
+		"maxWidth": p_width,
+		"maxHeight": p_height
+	});
+};
+
+
+var er_format = function() {
 
 	//get the height and width of the window
 	var height = $(window).height();
 	var width = $(window).width();
 
-	var dimensions = page_dimensions(width, height);
+	var dimensions = page_dimensions(width, height)
 
-	alert(dimensions[0]);
-	alert(dimensions[1]);
-});
+    var ereader = $('#ereaderJS');
+    var manuscript = ereader.val().replace(/\n/g, '</p><p>');
+    manuscript = "<p>" + manuscript;
+    $('#er_output').html(manuscript);
+    $('#testform').hide();
 
+    //set up css on <p> element
+    $('p').css({
+    	"textIndent" : "50px",
+    	"margin" : "0",
+    	"padding" : "0"
+    })
 
+	er_page_setup(dimensions[0], dimensions[1], "#ejreaderJS");
+}
 
