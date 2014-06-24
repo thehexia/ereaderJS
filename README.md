1ec92226c3ce765210e7ed36ae50ee1f915322aa
hoang nguyen <hvn1@zips.uakron.edu>
2014-06-24 08:55:30 -0400

    Added page_dimensions() to calculate page size based on screen size.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+(c) Hoang Nguyen
+
+
+Javascript library for rendering HTML into a scrollable ebook format.
 
--- ereaderJS.js
+++ ereaderJS.js
@@ -4,8 +4,34 @@
 Allows you to turn plain text into XHTML scrollable book format
 */
 
-//To use ereader, you must assign an area with the id="ereaderJS"
+//scale_factor
+scale_factor = 0.5;
 
+//determine page widthxheight
+function page_dimensions(width, height) {
+
+	//return values
+	var p_width = 0;
+	var p_height = 0;
+
+	//if the height is greater than the width we can assume the screen is in portrait mode
+	// in this case we can use the whole screen for the page
+	if(height > width) {
+		p_width = width;
+		p_height = height;
+	}
+
+	//if the width is greater than the height we can assume the screen is in landscape mode
+	// in this case we should only use part of the screen for the page
+	else {
+		p_width = width * scale_factor;
+		p_height = height;
+	}
+
+	return [p_width, p_height];
+}
+
+//To use ereader, you must assign an area with the id="ereaderJS"
 $(document).ready(function() {
 	//variable for textarea containing the body of text
 	var ereader = $('#ereaderJS');
@@ -17,9 +43,11 @@ $(document).ready(function() {
 	var height = $(window).height();
 	var width = $(window).width();
 
-	//if the height is greater than the width we can assume the screen is in portrait mode
-	// in this case we can use the whole screen for the page
+	var dimensions = page_dimensions(width, height);
 
-	//if the width is greater than the height we can assume the screen is in landscape mode
-	// in this case we should only use part of the screen for the page
+	alert(dimensions[0]);
+	alert(dimensions[1]);
 });
+
+
+
